14906ab94b85be59708b446b1ebc4f2477c02e9b
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-07-02 20:44:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  
+  get '/reversename/:name' do 
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+  
+  get '/square/:number' do 
+    @number = params[:number]
+    "#{@number.to_i * @number.to_i}"
+  end
+  
+  get '/say/:number/:phrase' do 
+    @number = params[:number]
+    @phrase = params[:phrase]
+    @number.to_i.times {@phrase}
+end
 
 end
